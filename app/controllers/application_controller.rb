66c0ebe1601cc83aa61b942638c7dfc4a68519ba
mkim <x1michelleskim@gmail.com>
2019-07-02 15:32:01 -0700

    Create users table, model, and user auth, finish application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+
+    # protect_from_forgery with: :exception
+
+    helper_method :require_login, :logged_in?, :current_user
+
+    def current_user
+        @current_user ||= User.find_by_session_token(session[:session_token])
+    end
+
+    def require_login
+        # redirect_to new_session_url unless logged_in?
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logged_in?
+        !!current_user
+    end
+    
+    def logout!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
 end
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: './frontend/entry.jsx',
+    entry: './frontend/in_tents.jsx',
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
