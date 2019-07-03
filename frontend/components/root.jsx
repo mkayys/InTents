3daca8293e4c1b2d9f11a512c12bc0d9cb603e9a
mkim <x1michelleskim@gmail.com>
2019-07-03 16:42:56 -0700

    Change import filename from App to app

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
-import App from './App';
+import App from './app';
 
 const Root = ({ store }) => (
     <Provider store={store}>
