e6c33d19852d85ba3df838f884eef032a4608646
mkim <x1michelleskim@gmail.com>
2019-08-14 15:54:57 -0700

    Render a gif and text for the loading page

---
+++ frontend/components/loading/loading_icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingIcon = () => (
+    <div className='loading-page'>
+        <img src='https://in-tents-seed.s3-us-west-1.amazonaws.com/main_images/tent-love.gif' className='logo-gif' />
+
+        <div className='loading-text'>finding places to pitch your tent...</div>
+    </div>
+);
+
+export default LoadingIcon
