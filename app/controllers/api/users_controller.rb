618f9c7902fe2d9807be91b36052c5d8cfb66ad7
Michelle Kim <x1michelleskim@gmail.com>
2020-01-09 20:03:09 -0800

    Test going from camelCase to snake_case when hitting users controller upon signing up a user and was successful

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -19,6 +19,6 @@ class Api::UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :password, :zip_code)
+        params.require(:user).transform_keys(&:underscore).permit(:first_name, :last_name, :email, :password, :zip_code)
     end
 end
